2196059d237e1a2c8df065f5bcbc9b2686274ec7
Jamie <jamie@otixo.com>
2025-09-18 11:14:04 +0200

    use sessionstorage instead of localstorage so the idp are not hidden for future sessions

--- src/login/pages/getHideIdp.tsx
+++ src/login/pages/getHideIdp.tsx
@@ -7,14 +7,14 @@ export const getHideIdp = () => {
 
         if (searchParams.has("hideIdp")) {
             const hideIdp = searchParams.get("hideIdp");
-            localStorage.setItem("hideIdp", hideIdp ?? "");
+            sessionStorage.setItem("hideIdp", hideIdp ?? "");
             return hideIdp === "true";
         }
 
         /**
-         * Fall back to local storage
+         * Fall back to session storage
          */
-        return localStorage.getItem("hideIdp") === "true";
+        return sessionStorage.getItem("hideIdp") === "true";
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (ex) {
         return null;
